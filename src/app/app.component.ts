4a1ec1c83ac60652927caa651d6eea32796e11ce
Jorge Martín <jorgemaram@gmail.com>
2021-05-07 18:04:40 +0200

    pipe for slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: string = 'Capitán América';
-  
+  nombre: string = 'Jorge';
+  arreglo: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 }
