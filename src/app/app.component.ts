1ddccdbb718258d6b6d966a9da538e27f220d9fd
Jorge Martín <jorgemaram@gmail.com>
2021-05-07 18:51:16 +0200

    currency and json pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,15 @@ export class AppComponent {
   arreglo: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   PI: number = Math.PI;
   porcentaje: number = 0.2451;
+  salario: number = 123546.25;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 32,
+    direccion: {
+      calle: 'Pien',
+      casa: 20
+    }
+  }
 
 }
